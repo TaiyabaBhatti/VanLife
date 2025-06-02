159be64d0faae3290f36f972f10cb52c79e50f0f
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-06-02 18:56:37 +0500

    Loading animation and respective usage
    styles the .loading class and implemented in in the ImageComponent also
    with own ImageComponent functionality

---
+++ src/pages/VansPage/ImageComponent.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function ImageComponent({ src, alt, properties }) {
+  const [isLoaded, setLoaded] = useState(false);
+
+  const loadState = () => {
+    setLoaded(true);
+  };
+
+  return (
+    <>
+      <div
+        className={`${
+          isLoaded ? "!hidden" : "flex"
+        } size-40  items-center justify-center`}
+      >
+        <div className="loading size-10  rounded-full p-1">
+          <div className=" rounded-full w-full h-full bg-white"></div>
+        </div>
+      </div>
+
+      <img
+        src={src}
+        alt={alt}
+        onLoad={loadState}
+        className={`${isLoaded ? "block" : "!hidden"}   ${properties} `}
+      />
+    </>
+  );
+}
