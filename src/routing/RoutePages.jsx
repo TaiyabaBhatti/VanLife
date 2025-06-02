3e0d920f95a87dbdacc5865e74278ec179efe46f
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-06-02 18:58:27 +0500

    Making use of nested routes and index
    utilize what i have learnt for nested routing

--- src/routing/RoutePages.jsx
+++ src/routing/RoutePages.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Vans from '../pages/VansPage/VansPage'
-import VanDetails from '../pages/VansPage/VanDetails'
-import { makeServer } from '../config/server'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Vans from "../pages/VansPage/VansPage";
+import VanDetailedPage from "../pages/VansPage/DetaliedPage/VanDetailedPage";
+import { makeServer } from "../config/server";
+import VanDetails from "../pages/VansPage/DetaliedPage/VanDetails";
+import VanPhotos from "../pages/VansPage/DetaliedPage/VanPhotos";
+import VanPricing from "../pages/VansPage/DetaliedPage/VanPricing";
 
 makeServer();
 // makeServer();
 export default function RoutePages() {
   return (
-<Routes>
-<Route path='/' element={<Home/>}/>
-<Route path='/home' element={<Home/>}/>
-<Route path='/about' element={<About/>}/>
-<Route path='/vans' element={<Vans/>}/>
-<Route path='/vans/:id' element={<VanDetails/>} />
-</Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/vans" element={<Vans />} />
+      <Route path="/vans/:id" element={<VanDetailedPage />}>
+        <Route path="pricing" element={<VanPricing />} />
+        <Route path="photos" element={<VanPhotos />} />
+        <Route index element={<VanDetails />} />
+      </Route>
+    </Routes>
+  );
 }
